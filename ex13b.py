5f48a9148657048822270ccc2ad1e86055c0e430
Alexander Stage <jimmystagg@hotmail.com>
2014-09-14 21:32:28 +0100

    Exercise 13b and 13c

---
+++ ex13b.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+script, name1, name2 = argv
+
+print "The script is called:", script
+print "The first name is:", name1
+print "The second name is:", name2
---
+++ ex13c.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, name1, name2, name3, name4 = argv
+
+print "The script is called:", script
+print "The first name is:", name1
+print "The second name is:", name2
+print "The third name is:", name3
+print "The fourth name is:", name4 
+
+userinput = raw_input("what is your name? ")
+print "Your name is %s" % userinput
