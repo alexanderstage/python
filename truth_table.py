7c3017591d45236c453703f196682215c17b88cd
Alexander Stage <jimmystagg@hotmail.com>
2014-09-27 11:18:49 +0100

    Exercise 26 - 30

---
+++ truth_table.py
@@ -0,0 +1,33 @@
+NOT
+not False = True
+not True - False
+OR
+True or False = True
+True or True = True
+False or True = True
+False or False = False
+AND
+True and False = False
+True and True = True
+False and True = False
+False and False = False
+NOT OR
+not(True or False) = False
+not(True or True) = False
+not(False or True) = False
+not(False or False) = True
+NOT AND
+not(True or False) = True
+not(True or True) = False
+not(False or True) = True
+not(False or False) = True
+NOT EQUAL (!=)
+0 != 1 = True
+1 != 1 = False
+1 != 0 = True
+0 != 0 = False
+EQUAL
+0 == 1 = False
+1 == 1 = True
+1 == 0 = False
+0 == 0 = True
