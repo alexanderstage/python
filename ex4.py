4359d843950d76c4eb739693524a8b30121c9073
Alexander Stage <jimmystagg@hotmail.com>
2014-12-01 11:16:35 +0000

    updated to add total_workers

--- ex4.py
+++ ex4.py
@@ -7,6 +7,10 @@ cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
+boss = 1
+slaves = 20
+receptionists = 2
+total_workers = boss + slaves + receptionists
 
 print "There are", cars, "cars available."
 print "There are only", drivers, "drivers available."
@@ -14,3 +18,4 @@ print "There will be", cars_not_driven, "empty cars today."
 print "We can transport", carpool_capacity, "people today."
 print "We have", passengers, "to carpool today."
 print "We need to put about", average_passengers_per_car, "in each car."
+print "The total number of employies is", total_workers
